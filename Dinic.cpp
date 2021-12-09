d2d0806cc2153a778f29954d4069673867b1655d
fouzanuddin <77853243+fouzanuddin@users.noreply.github.com>
2021-12-08 21:15:22 -0600

    Final Submission
    Code with Datasets and edit that allows it to run for any number of nodes . Just change the file name and Node numbers

--- Dinic.cpp
+++ Dinic.cpp
@@ -1,32 +1,27 @@
-// C++ implementation of Dinic's Algorithm
+// C++ implementation of Dinic's Algorithm taken from geeksforgeeks link :https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/
 #include<bits/stdc++.h>
 #include<chrono>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 
-// A structure to represent a edge between
-// two vertex
 struct Edge
 {
-	int v ; // Vertex v (or "to" vertex)
-			// of a directed edge u-v. "From"
-			// vertex u can be obtained using
-			// index in adjacent array.
+	int v ; 
 
-	int flow ; // flow of data in edge
+	int flow ;
 
-	int C; // capacity
+	int C;
 
-	int rev ; // To store index of reverse
-			// edge in adjacency list so that
-			// we can quickly find it.
+	int rev ; 
 };
 
 // Residual Graph
 class Graph
 {
-	int V; // number of vertex
-	int *level ; // stores level of a node
+	int V;
+	int *level ;
 	vector< Edge > *adj;
 public :
 	Graph(int V)
@@ -36,17 +31,17 @@ public :
 		level = new int[V];
 	}
 
-	// add edge to the graph
+
 	void addEdge(int u, int v, int C)
 	{
-		// Forward edge : 0 flow and C capacity
+
 		Edge a{v, 0, C, adj[v].size()};
 
-		// Back edge : 0 flow and 0 capacity
+
 		Edge b{u, 0, 0, adj[u].size()};
 
 		adj[u].push_back(a);
-		adj[v].push_back(b); // reverse edge
+		adj[v].push_back(b); 
 	}
 
 	bool BFS(int s, int t);
@@ -54,18 +49,13 @@ public :
 	int DinicMaxflow(int s, int t);
 };
 
-// Finds if more flow can be sent from s to t.
-// Also assigns levels to nodes.
+
 bool Graph::BFS(int s, int t)
 {
 	for (int i = 0 ; i < V ; i++)
 		level[i] = -1;
 
-	level[s] = 0; // Level of source vertex
-
-	// Create a queue, enqueue source vertex
-	// and mark source vertex as visited here
-	// level[] array works as visited array also.
+	level[s] = 0;
 	list< int > q;
 	q.push_back(s);
 
@@ -79,8 +69,7 @@ bool Graph::BFS(int s, int t)
 			Edge &e = *i;
 			if (level[e.v] < 0 && e.flow < e.C)
 			{
-				// Level of current vertex is,
-				// level of parent + 1
+			
 				level[e.v] = level[u] + 1;
 
 				q.push_back(e.v);
@@ -88,48 +77,37 @@ bool Graph::BFS(int s, int t)
 		}
 	}
 
-	// IF we can not reach to the sink we
-	// return false else true
+
 	return level[t] < 0 ? false : true ;
 }
 
-// A DFS based function to send flow after BFS has
-// figured out that there is a possible flow and
-// constructed levels. This function called multiple
-// times for a single call of BFS.
-// flow : Current flow send by parent function call
-// start[] : To keep track of next edge to be explored.
-//		 start[i] stores count of edges explored
-//		 from i.
-// u : Current vertex
-// t : Sink
+
 int Graph::sendFlow(int u, int flow, int t, int start[])
 {
-	// Sink reached
+
 	if (u == t)
 		return flow;
 
-	// Traverse all adjacent edges one -by - one.
+
 	for ( ; start[u] < adj[u].size(); start[u]++)
 	{
-		// Pick next edge from adjacency list of u
+
 		Edge &e = adj[u][start[u]];
 									
 		if (level[e.v] == level[u]+1 && e.flow < e.C)
 		{
-			// find minimum flow from u to t
+
 			int curr_flow = min(flow, e.C - e.flow);
 
 			int temp_flow = sendFlow(e.v, curr_flow, t, start);
 
-			// flow is greater than zero
+
 			if (temp_flow > 0)
 			{
-				// add flow to current edge
+
 				e.flow += temp_flow;
 
-				// subtract flow from reverse edge
-				// of current edge
+			
 				adj[e.v][e.rev].flow -= temp_flow;
 				return temp_flow;
 			}
@@ -139,39 +117,36 @@ int Graph::sendFlow(int u, int flow, int t, int start[])
 	return 0;
 }
 
-// Returns maximum flow in graph
+
 int Graph::DinicMaxflow(int s, int t)
 {
-	// Corner case
+	
 	if (s == t)
 		return -1;
 
-	int total = 0; // Initialize result
+	int total = 0; 
 
-	// Augment the flow while there is path
-	// from source to sink
 	while (BFS(s, t) == true)
 	{
-		// store how many edges are visited
-		// from V { 0 to V }
+		
 		int *start = new int[V+1] {0};
 
-		// while flow is not zero in graph from S to D
+		
 		while (int flow = sendFlow(s, INT_MAX, t, start))
 
-			// Add path flow to overall flow
+			
 			total += flow;
 	}
 
-	// return maximum flow
+	
 	return total;
 }
 
-// Driver Code
+
 int main()
 {
-   		auto a = std::chrono::high_resolution_clock::now();
-	Graph g(6);
+   		
+	Graph g(1200);
 	/*g.addEdge(0, 1, 16 );
 	g.addEdge(0, 2, 13 );
 	g.addEdge(1, 2, 10 );
@@ -197,7 +172,7 @@ int main()
 	g.addEdge(0, 4, 10);
 */
 	// next exp
-	g.addEdge(0, 1, 10);
+	/*g.addEdge(0, 1, 10);
 	g.addEdge(0, 2, 10);
 	g.addEdge(1, 3, 4 );
 	g.addEdge(1, 4, 8 );
@@ -205,9 +180,21 @@ int main()
 	g.addEdge(2, 4, 9 );
 	g.addEdge(3, 5, 10 );
 	g.addEdge(4, 3, 6 );
-	g.addEdge(4, 5, 10 ); 
+	g.addEdge(4, 5, 10 ); */
 
-	cout << "Maximum flow " << g.DinicMaxflow(0, 5)<<endl;
+ifstream inputData;
+   int fn,tn,cap;
+   inputData.open("data1200.dat"); 
+   while(!inputData.eof()){
+        inputData >> fn;
+        inputData >> tn;
+        inputData >> cap;
+       
+        g.addEdge(fn,tn,cap);
+    }
+   inputData.close();
+auto a = std::chrono::high_resolution_clock::now();
+	cout << "Maximum flow " << g.DinicMaxflow(0, 1199)<<endl;
 	auto b = std::chrono::high_resolution_clock::now();
 	cout<<"Time Taken By Dinic Algorithm: "<<std::chrono::duration_cast<std::chrono::microseconds>(b-a).count()<<" microseconds"<< endl;	
 	return 0;
--- Ford.cpp
+++ Ford.cpp
@@ -1,46 +1,46 @@
-// C++ program for implementation of Ford Fulkerson
-// algorithm
+// Code taken from geeks for geeks link to code : https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+
 #include <iostream>
 #include <limits.h>
 #include <queue>
 #include <string.h>
 #include<chrono>
+#include <iostream>
+#include <fstream>
+
 using namespace std;
 
-// Number of vertices in given graph
-#define V 6
+#define V 1200
 
-/* Returns true if there is a path from source 's' to sink
-'t' in residual graph. Also fills parent[] to store the
-path */
+int count = 0;
+int vertArr[V][V] ; 
+
+	
 bool bfs(int rGraph[V][V], int s, int t, int parent[])
 {
-	// Create a visited array and mark all vertices as not
-	// visited
+	
 	bool visited[V];
 	memset(visited, 0, sizeof(visited));
 
-	// Create a queue, enqueue source vertex and mark source
-	// vertex as visited
+	
 	queue<int> q;
 	q.push(s);
 	visited[s] = true;
 	parent[s] = -1;
 
-	// Standard BFS Loop
+	
 	while (!q.empty()) {
 		int u = q.front();
 		q.pop();
 
 		for (int v = 0; v < V; v++) {
 			if (visited[v] == false && rGraph[u][v] > 0) {
-				// If we find a connection to the sink node,
-				// then there is no point in BFS anymore We
-				// just have to set its parent and can return
-				// true
+				
 				if (v == t) {
 					parent[v] = u;
+				
 					return true;
+					
 				}
 				q.push(v);
 				parent[v] = u;
@@ -49,89 +49,98 @@ bool bfs(int rGraph[V][V], int s, int t, int parent[])
 		}
 	}
 
-	// We didn't reach sink in BFS starting from source, so
-	// return false
+	
 	return false;
 }
+void displayMatrix(int v) {
+   int i, j;
+   for(i = 0; i < v; i++) {
+      for(j = 0; j < v; j++) {
+         cout << vertArr[i][j] << " ";
+      }
+      cout << endl;
+   }
+}
+void add_edge(int u, int v,int w) {       
+   vertArr[u][v] = w;
+   
+}
 
-// Returns the maximum flow from s to t in the given graph
 int fordFulkerson(int graph[V][V], int s, int t)
 {
 	int u, v;
 
-	// Create a residual graph and fill the residual graph
-	// with given capacities in the original graph as
-	// residual capacities in residual graph
-	int rGraph[V]
-			[V]; // Residual graph where rGraph[i][j]
-				// indicates residual capacity of edge
-				// from i to j (if there is an edge. If
-				// rGraph[i][j] is 0, then there is not)
-	for (u = 0; u < V; u++)
-		for (v = 0; v < V; v++)
+
+
+	int rGraph[V][V]; 
+
+
+	for (u = 0; u < V; u++){
+		for (v = 0; v < V; v++){
+
 			rGraph[u][v] = graph[u][v];
+}
+}
 
-	int parent[V]; // This array is filled by BFS and to
-				// store path
+	int parent[V]; 
 
-	int max_flow = 0; // There is no flow initially
-
-	// Augment the flow while there is path from source to
-	// sink
+	int max_flow = 0; 
 	while (bfs(rGraph, s, t, parent)) {
-		// Find minimum residual capacity of the edges along
-		// the path filled by BFS. Or we can say find the
-		// maximum flow through the path found.
+		
 		int path_flow = INT_MAX;
+	
+
 		for (v = t; v != s; v = parent[v]) {
 			u = parent[v];
 			path_flow = min(path_flow, rGraph[u][v]);
 		}
 
-		// update residual capacities of the edges and
-		// reverse edges along the path
+		
 		for (v = t; v != s; v = parent[v]) {
 			u = parent[v];
 			rGraph[u][v] -= path_flow;
 			rGraph[v][u] += path_flow;
 		}
 
-		// Add path flow to overall flow
 		max_flow += path_flow;
 	}
 
-	// Return the overall flow
 	return max_flow;
 }
 
-// Driver program to test above functions
+
 int main()
 
 {
-	// Let us create a graph shown in the above example
-	auto a = std::chrono::high_resolution_clock::now();
 
-	//int graph[V][V]
-	//	= { { 0, 16, 13, 0, 0, 0 }, { 0, 0, 10, 12, 0, 0 },
-	//		{ 0, 4, 0, 0, 14, 0 }, { 0, 0, 9, 0, 0, 20 },
-	//		{ 0, 0, 0, 7, 0, 4 }, { 0, 0, 0, 0, 0, 0 } };
-
-	//int graph[V][V]
-	//	= {{0,3,7,0,10,0},
-	//		{3,0,9,9,9,0 },
-	//			{7,9,0,0,9,4 },
-	//				{0,9,0,0,0,3 },
-	//					{10,9,9,0,0,7},
-	//						{0,0,4,3,7,0 }};
 	
-	int graph[V][V] ={{0,10,10,0,0,0},{10,0,2,4,8,0 },{10,2,0,0,9,0 },{0,4,0,0,6,10 },{0,8,9,6,0,10 },{0,0,0,10,10,0}};
-	
+      
+   int v = V;   
+   ifstream inputData;
+   int fn,tn,cap;
+   inputData.open("data1200.dat"); 
+   while(!inputData.eof()){
+        inputData >> fn;
+        inputData >> tn;
+        inputData >> cap;
+       
+        add_edge(fn,tn,cap);
+    }
+   inputData.close();
+ 
 
-	cout << "The maximum possible flow is "
-		<< fordFulkerson(graph, 0, 5)<<endl;
+	
+	
+	
+      auto a = std::chrono::high_resolution_clock::now();
+	cout << "The maximum possible flow is for nodes :"<< v <<" value :"
+		<< fordFulkerson(vertArr, 0, V-1)<<endl;
+
+
 	auto b = std::chrono::high_resolution_clock::now();
 
 	cout<<"Time Taken By Ford Fulkersons Algorithm: "<<std::chrono::duration_cast<std::chrono::microseconds>(b-a).count()<<" microseconds"<< endl;	
 
 	return 0;
 }
+
